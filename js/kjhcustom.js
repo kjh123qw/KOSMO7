a16c19c519d3ba7b147f6e77782314314138e7dc
jeongho kim <kjh123qwa@naver.com>
2019-09-12 17:00:12 +0900

    Login, Join Major Update
    css, jquery major update

--- js/kjhcustom.js
+++ js/kjhcustom.js
@@ -1,28 +1,40 @@
 $(function(){
+    var tempPlaceHolderTxt;
+    // 로그인 화면
     $('.btn-login').click(function(){
         $('#loginModule').show();
     });
-
     $('.loginBackground').click(function(){
         $('#loginModule').hide();
     });
-
     $('.layerClose').click(function(){
         $('#loginModule').hide();
     });
-    // 로그인 폼 선택 시 테두리
-    $('#loginId').on("focus", function(event){
-        $(event.target).css('border','1px solid #777');
+
+    // 회원가입 화면
+    $('.btn-join').click(function(){
+        $('#loginModule').hide();
+        $('#joinModule').show();
     });
-    $('#loginPwd').on("focus", function(event){
-        $(event.target).css('border','1px solid #777');
-    });
-    // 로그인 폼 선택 아웃 시 테두리 제거
-    $('#loginId').on("blur", function(event){
-        $(event.target).css('border','1px solid #eee');
-    });
-    $('#loginPwd').on("blur", function(event){
-        $(event.target).css('border','1px solid #eee');
+    $('.joinBackground').click(function(){
+        $('#joinModule').hide();
     });
 
+    // input 선택 시 처리
+    $('.focusFunc').on("focus", function(event){
+        lableOn($(event.target));
+    });
+    $('.focusFunc').on("blur", function(event){
+        lableOff($(event.target));
+    });
+    // input 선택 function
+    function lableOn($obj){
+        tempPlaceHolderTxt = $obj.attr('placeholder');
+        $obj.css('border','1px solid #ccc').attr('placeholder','');
+        $obj.prev().stop().show();
+    }
+    function lableOff($obj){
+        $obj.css('border','1px solid #eee').attr('placeholder',$obj.prev().text());
+        $obj.prev().stop().hide();
+    }
 });
