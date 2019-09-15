04df16a1ab9ed6fe70b03c0d95b6d415f45f95e9
jeongho kim <kjh123qwa@naver.com>
2019-09-15 19:21:00 +0900

    sTownMain update

--- smallTown/js/kjhSTCustom.js
+++ smallTown/js/kjhSTCustom.js
@@ -1,22 +1,57 @@
 $(function(){
-    
-    var swiper = new Swiper('.swiper-container', {
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false, 
-        },
-        loop: true,
-        slidesPerView: 3,
-        spaceBetween: 30,
-        
-        
-      });
+  bindMouse();  // bind 초기화
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false, 
+    },
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+  });
+  $('.sTownBox').each(function(i, e){
+    var txt = $(e).find('span').eq(1).text().slice(0, 140);  // 영어: 230, 한글: 140
+    var txtEndIndex = txt.lastIndexOf(' ', 230);
+    txt = txt.slice(0, txtEndIndex) + "...";
+    $(e).find('span').eq(1).text(txt);
+  });
+  // 마우스 오버 확대 메소드 bind
+  function bindMouse(){
+    $('.sTownBox a').bind('mouseover',function(e){
+      zoomImg($(e.target));
+    });
+    $('.sTownBox a').bind('mouseout',function(e){
+      zoomOutImg($(e.target));
+    });
+  }
+  // 이미지 확대 메소드
+  function zoomImg($e){
+    $e.next().children('img').stop().animate({
+      height: '120%',
+      top: '-10%'
+    }, 1500);
+    // $e.next().next().children('span').eq('0').stop().animate({
+    //   fontSize: '26px'
+    // }, 500);
+  }
+  // 이미지 축소 메소드
+  function zoomOutImg($e){
+    $e.next().children('img').stop().animate({
+      height: '100%',
+      top: '0'
+    }, 100);
+    // $e.next().next().next().stop().animate({
+    //   height: '35px',
+    //   fontSize: '16px'
+    // }, 100);
+  }
 });
+      // console.log($(e.target) + ' ' + $(e.target).parent('a').parent('div').width() + ' / ' +$(e.target).parent('a').parent('div').height());
