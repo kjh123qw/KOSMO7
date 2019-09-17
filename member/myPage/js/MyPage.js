8951ce1be1bbc1504466601deab80d2d655917d9
MarineOnce <47581819+MarineOnce@users.noreply.github.com>
2019-09-17 17:55:20 +0900

    member page

---
+++ member/myPage/js/MyPage.js
@@ -0,0 +1,34 @@
+$(function(){
+    $('.MyMatsuri').click(function(){
+        $('div#MyPage').remove(); 
+        $('section').append('<div id="content"></div>'); 
+        $(document).ready(function(){
+            document.getElementById("content").innerHTML='<object type="text/html" data="./mMyMatsuri.html"></object>';
+        })
+    });
+    
+
+    
+});
+
+
+
+
+
+// $(document).ready(function(){
+//     document.getElementById("footer").innerHTML='<object type="text/html" data="footer.html"></object>';
+// })
+
+
+// $(document).ready( function() {
+
+// 	$(".MyMatsuri").load("mMyMasuri.html");
+// }
+
+// $( 'div' ).load( 'a.html p' );
+
+{/* <script>
+      $( document ).ready( function() {
+        $( '#xy' ).load( 'load-02.html #ab' );
+      } );
+</script> */}
