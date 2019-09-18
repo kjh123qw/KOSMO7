ef0c2d4ec21a1420a81dd338c19e3d012d7e4f9f
YUJISANG <54089426+yujisang93@users.noreply.github.com>
2019-09-18 19:46:23 +0900

    admin 수정~

--- admin/js/adminMain.js
+++ admin/js/adminMain.js
@@ -4,25 +4,59 @@ $(function(){
 
    
         
-    $('.barIcon').click(function(){
-        var color = $('.barIcon').css('color');
-        console.log(color)
+    // $('.barIcon').click(function(){
+    //     var color = $('.barIcon').css('color');
+    //     console.log(color)
 
-        if(color == 'rgb(33, 37, 41)'){
-            $('.barIcon').css('color','white')
-            $('.aside').stop().animate({left:-400})
-            $('.aside').css('display','none')
-            // $('#section').css('transition-duration','2s')
-            $('#section').attr('class',"col-sm-12");
+    //     if(color == 'rgb(33, 37, 41)'){
+    //         $('.barIcon').css('color','white')
+    //         $('.aside').stop().animate({left:-400})
+    //         $('.aside').css('display','none')
+    //         // $('#section').css('transition-duration','2s')
+    //         $('#section').attr('class',"col-sm-12");
 
+    //     }else{
+    //         $('.barIcon').css('color','rgb(33, 37, 41)')
+    //         $('.aside').stop().animate({left:0})
+    //         $('.aside').css('display','block')
+    //         // $('#section').css('transition-duration','2s')
+    //         $('#section').attr('class',"col-sm-9");
+    //     }
+    // })
+
+    $('#ulDiv li').mouseenter(function(){
+
+        var name = $(this).attr('name');
+        if(name == 'main'){
+            return false;
+        }
+
+        $(this).addClass('shake');
+        
+    })
+
+    $('#ulDiv li').mouseleave(function(){
+        $(this).removeClass('shake');
+        
+    })
+
+    $('#testNone').css("display" ,"none");
+    $(window).resize(function(){
+        if($(window).width() < 768){
+            $('#wrap').css("display" ,"none");
+            $('#testNone').css("display" ,"block");
         }else{
-            $('.barIcon').css('color','rgb(33, 37, 41)')
-            $('.aside').stop().animate({left:0})
-            $('.aside').css('display','block')
-            // $('#section').css('transition-duration','2s')
-            $('#section').attr('class',"col-sm-9");
+            $('#wrap').css("display" ,"block");
+            $('#testNone').css("display" ,"none");
         }
     })
+
+    
+    
+
+    
+
+    
 
     
 
