46916c490c2e27e1057177945e69d377871e93d6
kjh123qw <54471296+kjh123qw@users.noreply.github.com>
2019-09-20 18:33:33 +0900

    main update

--- js/commonCustom.js
+++ js/commonCustom.js
@@ -2,10 +2,20 @@ $(function(){
     $('#menuBtn').click(
         function(){
             $('#navigation_m').toggle();
-            if ($('#menuBtn').text() == 'M')
+            if ($('#menuBtn').text() == 'M'){
                 $('#menuBtn').text('X');
-            else if ($('#menuBtn').text() == 'X')
+                $('header').css({
+                    "height": "320px",
+                    "background-image": "none",
+                    "background-color": "#444"
+                });
+            }
+            else if ($('#menuBtn').text() == 'X'){
                 $('#menuBtn').text('M');
+                $('header').css({
+                    "height": "180px"
+                }).removeAttr('style');
+            }
         }
     );
 })
