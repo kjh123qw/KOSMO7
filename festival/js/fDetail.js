c58d99072aada7eaaa87f5142853af0c4523621d
YUJISANG <54089426+yujisang93@users.noreply.github.com>
2019-10-10 19:00:45 +0900

    축제

--- festival/js/fDetail.js
+++ festival/js/fDetail.js
@@ -1,5 +1,27 @@
 $(function(){
 
+    $( window ).resize(function() {
+        //창크기 변화 감지
+        var divWidth1 = $('.imgList div img').width();
+        $('.imgList div img').height(divWidth1);
 
+        var divWidth2 = $('.imgDiv img').width();
+        $('.imgDiv img').height(divWidth2);
+
+        var modalWidth = $(window).width();
+        if(modalWidth < 993){
+            $( '.imgList .col-sm-3' ).removeAttr( 'data-toggle' );
+            // $( '.imgList .col-sm-3' ).removeAttr( 'data-target' );
+        }else{
+            $( '.imgList .col-sm-3' ).attr('data-toggle','modal')
+        }
+     });
+
+        var divWidth1 = $('.imgList div img').width();
+        $('.imgList div img').height(divWidth1);
+        var divWidth2 = $('.imgDiv img').width();
+        $('.imgDiv img').height(divWidth2);
+        
+        
 
 })
