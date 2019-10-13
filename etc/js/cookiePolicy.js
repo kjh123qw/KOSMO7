1234e6359334451e9233900a5ca594a4947eab22
MarineOnce <47581819+MarineOnce@users.noreply.github.com>
2019-10-13 16:07:44 +0900

    privacy, cookie

---
+++ etc/js/cookiePolicy.js
@@ -0,0 +1,13 @@
+$(function(){
+    cookiefunc();
+});
+
+function cookiefunc(){
+    $('.cookie1').click(function(){
+        $('.content1').toggle();
+    });
+
+    $('.cookie2').click(function(){
+        $('.content2').toggle();
+    });
+}
